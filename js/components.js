6dfd14996002bb79b0a8dbd1c91372cd83bbc33e
Lucas <metallucas.ds@gmail.com>
2013-07-03 17:34:56 -0300

    Update components

--- js/components.js
+++ js/components.js
@@ -32,5 +32,13 @@ angular.module('components', []).
     }
   }).
   directive('pane', function() {
-
+    return {
+      require: '^tabs',
+      restrict: 'E',
+      transclude: true,
+      scope: { title: '@' },
+      link: function(scope, element, attrs, tabsCtrl) {
+        tabsCtrl.addPane(scope);
+      },
+    }
   });
